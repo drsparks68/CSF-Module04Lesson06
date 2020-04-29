53065193e914af7f042447c37fe6f99824ba45ee
Derek Sparks <drsparks@gmail.com>
2020-04-29 16:12:16 -0700

    ss

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using DemoLibrary;
+using DemoLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ConsoleUI
 {
@@ -10,7 +13,24 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+
+            PersonModel person = new PersonModel();
+            person.FirstName = "Bob";
+            person.LastName = "Smith";
+
+
+            double rez = Calculation.Add(7, 3);
+            Console.WriteLine(rez);
+            Console.WriteLine(person.FirstName);
             Console.ReadLine();
         }
     }
 }
+
+
+    //From this point on, only place code for user interface in Program.cs
+    //Everything else should go into a class library
+
+    //Pro-Tip: 
+
+    //Homework: 
